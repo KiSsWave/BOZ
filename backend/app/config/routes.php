c6ecb88b79f8e6e26663e6728bb134e90c4d400c
Gaetan66 <127093056+Gaetan66@users.noreply.github.com>
2024-12-16 11:10:32 +0100

    ajout config

---
+++ backend/app/config/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+
+return function( \Slim\App $app):\Slim\App {
+
+    return $app;
+};
+
