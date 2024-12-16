0e7b5d554c54e752f0273059ae938c0da2806c8f
jeandidi3u <jeandidier.clement@gmail.com>
2024-12-16 13:25:14 +0100

    a

--- backend/app/config/dependencies.php
+++ backend/app/config/dependencies.php
@@ -1,3 +1,30 @@
 <?php
 
-return[];
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use DotEnv\DotEnv;
+
+$dbName = 'boz';
+$dotenv = Dotenv::createImmutable(__DIR__ .  'dbconnexion.env');
+$dotenv->load();
+
+
+$dsn = "pgsql:host=" . $_ENV['DB_HOST'] . ';port=' . $_ENV['DB_PORT'] . ';dbname=' . $dbName;
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+
+try {
+    $pdo = new PDO($dsn, $user, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "check";
+    return $pdo;
+} catch (Exception $e) {
+    throw new \RuntimeException("Erreur lors de la connexion à la bd : " . $e->getMessage());
+}
+
+
+
+
+
+
+
