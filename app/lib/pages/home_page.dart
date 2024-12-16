348019e6f360254e262262521a1ebd25440aff1c
Okiles <colsonlenny5@gmail.com>
2024-12-16 19:54:00 +0100

    adding buttons for history

--- app/lib/pages/home_page.dart
+++ app/lib/pages/home_page.dart
@@ -8,23 +8,83 @@ class HomePage extends StatelessWidget {
     return SafeArea(
       child: Column(
         children: [
-          Container(
-            margin: EdgeInsets.only(top: 20),
-            child: const Center(
-              child: Text(
-                "Solde : 0 €",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
+          SizedBox(
+            width: 200,
+            height: 200,
+            child: Container(
+              margin: const EdgeInsets.only(top: 20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                border: Border.all(color: Colors.black),
+                borderRadius:
+                    BorderRadius.circular(24), // Ajout du borderRadius
+              ),
+              child: const Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      "120 €",
+                      style: TextStyle(
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Solde",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.normal,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
           ),
-          const Center(
-            child: Text(
-              "Bienvenue sur BOZ !",
-              style: TextStyle(color: Colors.grey),
-            ),
+          const SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton.icon(
+                onPressed: () {
+                  print("Rechargements");
+                },
+                icon: const Icon(
+                  Icons.history,
+                  size: 24,
+                ),
+                label: const Text(
+                  "Rechargements",
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                ),
+                style: ElevatedButton.styleFrom(
+                  iconColor: Colors.black,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                ),
+              ),
+              ElevatedButton.icon(
+                onPressed: () {
+                  print("Transactions");
+                },
+                icon: const Icon(
+                  Icons.account_balance_wallet,
+                  size: 24,
+                ),
+                label: const Text(
+                  "Transactions",
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                ),
+                style: ElevatedButton.styleFrom(
+                  iconColor: Colors.black,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                ),
+              ),
+            ],
           ),
         ],
       ),
