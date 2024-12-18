e8e6d27f13ab2b64631ec580da91e9041e17b542
Okiles <colsonlenny5@gmail.com>
2024-12-18 17:54:59 +0100

    fixing register

--- app/lib/pages/qr_page.dart
+++ app/lib/pages/qr_page.dart
@@ -1,1 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+
+class QRPage extends StatefulWidget {
+  const QRPage({super.key});
+
+  @override
+  State<QRPage> createState() => _QRPageState();
+}
+
+class _QRPageState extends State<QRPage> {
+
+  @override
+  void initState() {
+    super.initState();
+    scanQR();
+  }
+
+  Future<void> scanQR() async {
+    String scanResult;
+    try {
+      scanResult = await FlutterBarcodeScanner.scanBarcode(
+          "#ff6666", "Cancel", true, ScanMode.QR);
+      print(scanResult);
+    } on PlatformException {
+      scanResult = "Failed to get platform version.";
+    }
+    if (!mounted) return;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
