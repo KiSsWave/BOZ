ab306a61700da6440f52cc07c0c519bab6c6ce0f
Okiles <colsonlenny5@gmail.com>
2024-12-19 08:40:37 +0100

    Signin

--- app/lib/pages/home_page.dart
+++ app/lib/pages/home_page.dart
@@ -1,92 +1,128 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:boz/services/remote_service.dart';
+import 'package:boz/models/transaction.dart';
+
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  double balance = 0.0;
+  late String lastUpdateFormatted = "";
+  List<Transaction> allTransactions = [];
+  List<Transaction> displayedTransactions = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchBalance();
+  }
+
+  Future<void> _fetchBalance() async {
+    try {
+      final response = await RemoteService().fetchBalance();
+      final data = jsonDecode(response.body);
+      setState(() {
+        balance = data['balance'];
+        final now = DateTime.now();
+        lastUpdateFormatted = "${now.day}/${now.month}/${now.year}";
+      });
+    } catch (e) {
+      // Handle error gracefully
+      print('Error fetching balance: $e');
+    }
+  }
+
+  List<Transaction> _filterTransactions(bool isRecharge) {
+    displayedTransactions = isRecharge ? allTransactions.where((t) => t.amount > 0).toList() : allTransactions.where((t) => t.amount < 0).toList();
+    return displayedTransactions;
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Column(
-        children: [
-          SizedBox(
-            width: 200,
-            height: 200,
-            child: Container(
-              margin: const EdgeInsets.only(top: 20),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(color: Colors.black),
-                borderRadius:
-                    BorderRadius.circular(24), // Ajout du borderRadius
-              ),
-              child: const Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      "120 €",
-                      style: TextStyle(
-                        fontSize: 30,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    Text(
-                      "Solde",
-                      style: TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.normal,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      child: RefreshIndicator(
+        onRefresh: _fetchBalance,
+        color: Colors.black,
+        backgroundColor: Colors.grey[300],
+        child: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: Column(
             children: [
-              ElevatedButton.icon(
-                onPressed: () {
-                  print("Rechargements");
-                },
-                icon: const Icon(
-                  Icons.history,
-                  size: 24,
+              Container(
+                width: 200,
+                height: 200,
+                margin: const EdgeInsets.only(top: 20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(color: Colors.black),
+                  borderRadius: BorderRadius.circular(24),
                 ),
-                label: const Text(
-                  "Rechargements",
-                  style: TextStyle(fontSize: 16, color: Colors.black),
-                ),
-                style: ElevatedButton.styleFrom(
-                  iconColor: Colors.black,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "$balance €",
+                        style: const TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      Text(
+                        "Solde au $lastUpdateFormatted",
+                        style: const TextStyle(
+                          fontSize: 16,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-              ElevatedButton.icon(
-                onPressed: () {
-                  print("Transactions");
-                },
-                icon: const Icon(
-                  Icons.account_balance_wallet,
-                  size: 24,
-                ),
-                label: const Text(
-                  "Transactions",
-                  style: TextStyle(fontSize: 16, color: Colors.black),
-                ),
-                style: ElevatedButton.styleFrom(
-                  iconColor: Colors.black,
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-                ),
+              const SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  _buildActionButton(
+                    icon: Icons.history,
+                    label: "Rechargements",
+                    onPressed: () => print("Rechargements"),
+                  ),
+                  _buildActionButton(
+                    icon: Icons.account_balance_wallet,
+                    label: "Transactions",
+                    onPressed: () => print("Transactions"),
+                  ),
+                ],
               ),
             ],
           ),
-        ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildActionButton({
+    required IconData icon,
+    required String label,
+    required VoidCallback onPressed,
+  }) {
+    return ElevatedButton.icon(
+      onPressed: onPressed,
+      icon: Icon(icon, size: 24),
+      label: Text(
+        label,
+        style: const TextStyle(fontSize: 16, color: Colors.black),
+      ),
+      style: ElevatedButton.styleFrom(
+        iconColor: Colors.black,
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 12),
       ),
     );
   }
--- app/lib/services/remote_service.dart
+++ app/lib/services/remote_service.dart
@@ -1,10 +1,11 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class RemoteService {
-  final storage = FlutterSecureStorage();
+  final storage = const FlutterSecureStorage();
   final baseUrl = dotenv.env['BASE_URL']; // Chargement de l'URL de base
 
   Future<http.Response> registerUser(
@@ -41,7 +42,7 @@ class RemoteService {
             'Login failed: ${response.body}', response.statusCode);
       }
 
-      var token = response.headers[HttpHeaders.authorizationHeader];
+      var token = jsonDecode(response.body)['token'];
 
       if (token == null) {
         return http.Response('Missing authorization token', 401);
@@ -62,5 +63,59 @@ class RemoteService {
   
   Future<bool> isConnected() async {
     return await storage.read(key: 'jwt') != null;
+  }
+
+  Future<http.Response> fetchBalance() async {
+    try {
+      var client = http.Client();
+      var uri = Uri.parse('$baseUrl/balance'); // Utilisation de baseUrl
+
+      var token = await storage.read(key: 'jwt');
+
+      if (token == null) {
+        return http.Response('Missing authorization token', 401);
+      }
+
+      var response = await client.get(uri, headers: {
+        HttpHeaders.authorizationHeader: 'Bearer $token',
+      });
+
+      if (response.statusCode != 200) {
+        return http.Response(
+            'Failed to fetch balance: ${response.body}', response.statusCode);
+      }
+
+      return response;
+    } catch (e) {
+      print("Error during balance fetch: $e");
+      return http.Response('Error during balance fetch: $e', 500);
+    }
+  }
+
+  Future<http.Response> fetchTransactions() async {
+    try {
+      var client = http.Client();
+      var uri = Uri.parse('$baseUrl/transactions'); // Utilisation de baseUrl
+
+      var token = await storage.read(key: 'jwt');
+
+      if (token == null) {
+        return http.Response('Missing authorization token', 401);
+      }
+
+      var response = await client.get(uri, headers: {
+        HttpHeaders.authorizationHeader: 'Bearer $token',
+      });
+
+      if (response.statusCode != 200) {
+        return http.Response(
+            'Failed to fetch transactions: ${response.body}', response.statusCode);
+      }
+
+      return response;
+    } catch (e) {
+      print("Error during transactions fetch: $e");
+      return http.Response('Error during transactions fetch: $e', 500);
+    }
   }
 }
